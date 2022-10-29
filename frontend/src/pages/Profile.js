064ae57de564f5a248ce9c5829fef42aa259681f
Arkajit Datta <arkajitdatta2010@gmail.com>
2022-10-30 01:19:08 +0530

    added maps

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import NavBar from './NavBar'
+import './../style/Commentary.css'
 
 const Profile = () => {
   return (
     <>
         <div className='viewContainer'>
+        
             <NavBar />
         </div>
+        
     </>
   )
 }
