f63cf00de96baf403590e8beaa6176ae070fac3d
naveen-manda <naveen.manda69@gmail.com>
2022-10-29 22:18:05 +0530

    update

---
+++ frontend/src/pages/commentary.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Commentary = () => {
+  return (
+    <>
+        <div className='viewContainer'>
+            
+        </div>
+    </>
+  )
+}
+
+export default Commentary
