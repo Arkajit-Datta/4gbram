1ce10bac294200344db503c0bed6c7a7bc90c439
naveen-manda <naveen.manda69@gmail.com>
2022-10-30 11:30:36 +0530

    ui final

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-var Data = {"url" : "https://f49e-2401-4900-4e23-dae4-e6fd-561c-a11b-7da2.in.ngrok.io/goals"};
+var Data = {"url" : "https://7f6e-2401-4900-4e23-dae4-e6fd-561c-a11b-7da2.in.ngrok.io/goals"};
 
 export default Data;
